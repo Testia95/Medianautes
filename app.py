e43cee4b80d408bb9dcd4065cf382802c44accd3
Testia95 <gptestia@proton.me>
2025-04-11 17:28:21 +0200

    Medianautes V2.2

--- app.py
+++ app.py
@@ -9,18 +9,17 @@ from datetime import datetime
 
 app = Flask(__name__)
 
-# Charger le fichier JSON contenant la configuration des médias
+# Chargement des infos médias
 with open('media_data.json', 'r', encoding='utf-8') as f:
     MEDIA_INFO = json.load(f)
 
-# Dictionnaire pour stocker les vidéos récupérées pour chaque média
+# Dictionnaire contenant les vidéos récupérées pour chaque média
 VIDEOS_BY_MEDIA = {}
 
 def fetch_videos_for_all_medias():
     """
-    Récupère les vidéos via les flux RSS YouTube de chaque média.
-    Pour chaque média, on construit une liste de vidéos (titre, lien, date, miniature),
-    triée par date décroissante.
+    Récupère les vidéos de chaque média via feedparser.
+    Stocke le résultat dans VIDEOS_BY_MEDIA sous forme de listes triées par date.
     """
     global VIDEOS_BY_MEDIA
     VIDEOS_BY_MEDIA = {}
@@ -31,115 +30,39 @@ def fetch_videos_for_all_medias():
             feed = feedparser.parse(flux_url)
             media_videos = []
             for entry in feed.entries:
-                video_title = entry.title
-                video_link = entry.link
+                title = entry.title
+                link = entry.link
                 published_parsed = entry.published_parsed
+
+                # Convertir published_parsed en datetime pour faciliter le tri
                 if published_parsed:
                     published_date = datetime(*published_parsed[:6])
                 else:
                     published_date = datetime.now()
-                
+
+                # Extraire l'ID vidéo pour la miniature
                 video_id = None
-                if "watch?v=" in video_link:
-                    video_id = video_link.split("watch?v=")[1]
-                
+                if "watch?v=" in link:
+                    video_id = link.split("watch?v=")[1]
+
                 thumbnail_url = None
                 if video_id:
                     thumbnail_url = f"https://img.youtube.com/vi/{video_id}/hqdefault.jpg"
 
                 video_data = {
-                    "title": video_title,
-                    "link": video_link,
+                    "title": title,
+                    "link": link,
                     "published": published_date,
                     "thumbnail": thumbnail_url
                 }
                 media_videos.append(video_data)
+            
+            # Tri par date décroissante
             media_videos.sort(key=lambda x: x["published"], reverse=True)
             VIDEOS_BY_MEDIA[media_name] = media_videos
 
-# Appel initial pour remplir VIDEOS_BY_MEDIA
+# Premier remplissage
 fetch_videos_for_all_medias()
 
-# Context Processor pour rendre disponibles dans tous les templates
 @app.context_processor
-def inject_sidebar_data():
-    """
-    Injecte dans chaque template la liste triée des médias et pour chacun les 5 dernières vidéos.
-    Ceci permet d'afficher une barre latérale uniforme sur toutes les pages.
-    """
-    media_names_sorted = sorted(MEDIA_INFO.keys())
-    last_5_by_media = {}
-    for media_name in media_names_sorted:
-        media_videos = VIDEOS_BY_MEDIA.get(media_name, [])
-        last_5_by_media[media_name] = media_videos[:5]
-    return dict(media_names_sorted=media_names_sorted, last_5_by_media=last_5_by_media)
-
-@app.route('/')
-def home():
-    """
-    Page d'accueil :
-    - Affiche les 12 dernières vidéos (tous médias confondus)
-    - Affiche 10 dernières vidéos par orientation (Gauche, Centre, Droite, Autres)
-    La barre latérale (déployable) est affichée depuis le template de base.
-    """
-    all_videos = []
-    for media_name, videos in VIDEOS_BY_MEDIA.items():
-        for video in videos:
-            all_videos.append({
-                "media_name": media_name,
-                "title": video["title"],
-                "link": video["link"],
-                "published": video["published"],
-                "thumbnail": video["thumbnail"],
-                "orientation": MEDIA_INFO[media_name]["orientation"]
-            })
-    all_videos.sort(key=lambda x: x["published"], reverse=True)
-    last_12_videos = all_videos[:12]
-
-    videos_by_orientation = {"Gauche": [], "Centre": [], "Droite": [], "Autres": []}
-    for vid in all_videos:
-        orientation = vid["orientation"]
-        if orientation in videos_by_orientation:
-            videos_by_orientation[orientation].append(vid)
-        else:
-            videos_by_orientation["Autres"].append(vid)
-    for orientation in videos_by_orientation:
-        videos_by_orientation[orientation] = videos_by_orientation[orientation][:10]
-
-    return render_template('index.html',
-                           last_12_videos=last_12_videos,
-                           videos_by_orientation=videos_by_orientation)
-
-@app.route('/media/<media_name>')
-def media_page(media_name):
-    """
-    Page spécifique à un média.
-    Affiche les informations détaillées (orientation, PDG, description, intérêts économiques)
-    et la liste complète (ou en partie) des vidéos du média.
-    La barre latérale reste identique à celle définie dans le template de base.
-    """
-    info = MEDIA_INFO.get(media_name, {})
-    orientation = info.get("orientation", "Inconnu")
-    pdg = info.get("pdg", "Inconnu")
-    description = info.get("description", "")
-    interets_eco = info.get("interets_economiques", "")
-    media_videos = VIDEOS_BY_MEDIA.get(media_name, [])
-
-    return render_template('media.html',
-                           media_name=media_name,
-                           orientation=orientation,
-                           pdg=pdg,
-                           description=description,
-                           interets_economiques=interets_eco,
-                           media_videos=media_videos)
-
-@app.route('/refresh')
-def refresh_feeds():
-    """
-    Route pour rafraîchir manuellement les flux RSS.
-    """
-    fetch_videos_for_all_medias()
-    return redirect(url_for('home'))
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+def inject_sidebar_
